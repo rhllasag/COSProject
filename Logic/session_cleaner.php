24eac8c4bb1fb75ddc45d5c362a53fe33f0d8d88
Micael Farinha <micaes_3323@hotmail.com>
2018-05-17 18:14:08 +0100

    Test;

--- Logic/session_cleaner.php
+++ Logic/session_cleaner.php
@@ -3,8 +3,13 @@
 namespace qos\Logic;
 
 if (!empty($_GET['clear_session'])) {
-    unset($_SESSION['facebook_only']);
-    unset($_SESSION['linked_in_only']);
+    if (!empty($_SESSION['facebook_only'])) {
+        unset($_SESSION['facebook_only']);
+    }
+
+    if (!empty($_SESSION['linked_in_only'])) {
+        unset($_SESSION['linked_in_only']);
+    }
 
     if (!empty($_SESSION['facebook_active'])) {
         unset($_SESSION['facebook_active']);
